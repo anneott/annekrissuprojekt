6cbcf6db092368215610224c3e33fb847b45bf4e
Kristiina <kristiina.uusna@gmail.com>
2016-03-11 19:59:47 +0200

    proovin tekitada mängijat :D

---
+++ src/Mängija.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 11.03.2016.
+ */
+public class Mängija {
+    private String nimi;
+
+    public Mängija(String nimi) {
+        super();
+        this.nimi = nimi;
+    }
+
+}
