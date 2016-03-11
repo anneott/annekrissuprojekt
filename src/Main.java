d8393bf5a42f3ee119cc8867d3ce39fdbfdb8fbd
Anne <anneott5@gmail.com>
2016-03-11 19:18:46 +0200

    tervitasin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("TERE! JOUUUU ;::ÖÖAS");
+        System.out.println("tereeeee!!!");
         System.out.println("TErvitaab kristiiiiiiiiiiina");
         System.out.println("tereeeee");
     }
