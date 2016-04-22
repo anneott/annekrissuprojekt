c85f5977f83b4b65a98771922c65ff3bc6d913de
Kristiina <kristiina.uusna@gmail.com>
2016-04-22 14:04:58 +0300

    Proovisin graafilieks teha andmete sisestuse, vigade teatamise ja veereta nupu.

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,164 @@
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    public void start(Stage peaLava) {
+
+        // GridPane loomine (sarnane nagu Group)
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        peaLava.setTitle("Täringumäng"); // lava tiitelribale pannakse tekst
+        Scene stseen1 = new Scene(grid, 300, 300, Color.WHITE);
+        peaLava.setScene(stseen1);
+        peaLava.show(); // lava tehakse nähtavaks
+
+        tegevusEkraanil(grid, peaLava);
+    }
+
+    public static Node tegevusEkraanil(GridPane grid, Stage peaLava) {
+        // Esimese mängija nime kast
+        final TextField mängijanr1 = new TextField();
+        mängijanr1.setPromptText("Esimese mängija nimi");
+        GridPane.setConstraints(mängijanr1, 0, 0);
+        grid.getChildren().add(mängijanr1);
+
+        Mängija mängija1 = new Mängija(mängijanr1.getText());
+
+        // Teise mängija nime kast
+        final TextField mängijanr2 = new TextField();
+        mängijanr2.setPromptText("Teise mängija nimi");
+        GridPane.setConstraints(mängijanr2, 0, 1);
+        grid.getChildren().add(mängijanr2);
+
+        Mängija mängija2 = new Mängija(mängijanr2.getText());
+
+        // Visete arvu määramiseks kast
+        final TextField viskeArvMängus = new TextField();
+        // Hetke on visete arv piiramatu, peab veel kontrollima, et ei oleks
+        // NULL !!!!
+        viskeArvMängus.setPromptText("Visete arv");
+        GridPane.setConstraints(viskeArvMängus, 0, 2);
+        grid.getChildren().add(viskeArvMängus);
+
+        // Alusta nupp
+        Button submit = new Button("Alusta");
+        GridPane.setConstraints(submit, 1, 0);
+        grid.getChildren().add(submit);
+
+        // Puhasta nupp
+        Button clear = new Button("Puhasta");
+        GridPane.setConstraints(clear, 1, 1);
+        grid.getChildren().add(clear);
+
+        // Cancel nupp
+        Button cancel = new Button("Lahku");
+        GridPane.setConstraints(cancel, 1, 2);
+        grid.getChildren().add(cancel);
+
+        submit.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                // kui kõik väljad pole täidetud, siis ei luba mängu alustada
+                if (mängijanr1.getText().trim().isEmpty() || mängijanr2.getText().trim().isEmpty()
+                        || viskeArvMängus.getText().trim().isEmpty()) {
+                    peaLava.hide();
+                    Stage uus = new Stage();
+                    String tekst = null;
+                    tekst = "Kõik väljad pole täidetud !";
+                    Button ok = new Button("Proovi uuesti"); // luuakse nupp
+                    // nupu paigutus annab soovida !!!
+                    Label label = new Label(tekst, ok);
+                    Scene stseen2 = new Scene(label, 300, 100, Color.AQUAMARINE);
+                    uus.setScene(stseen2);
+                    uus.show();
+                    ok.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        public void handle(MouseEvent me) {
+                            uus.hide();
+                            peaLava.show();
+                        }
+                    });
+                }
+
+                try { // Proovib kas sisestatud numbrit on võimalik int tüüpi
+                    // muutujaks muuta.
+                    int viseteArvInt = Integer.parseInt(viskeArvMängus.getText());
+                    //OTSIB MILLEGI PÄRAST ENNE VÕITJAT, KUI ALUSATB MÄNGUGA !!
+                    MänguKäik uusMäng = new MänguKäik(0,viseteArvInt,0);
+                    uusMäng.alustaMänguga(mängija1, mängija2, grid, peaLava);
+
+                    int summa1 = uusMäng.getPunktideSumma1();
+                    int summa2 = uusMäng.getPunktideSumma2();
+
+                    Võitja võitja = new Võitja();
+
+                    String võitjanimi = võitja.leiaVõitja(mängija1, mängija2,summa1, summa2);
+
+                    Stage uus = new Stage();
+                    String tekst = mängija1 + " punktisumma : " + summa1 + " ja " +mängija2 + " punktisumma : " + summa2 +". Võitja on : " + võitjanimi;
+                    Label label = new Label(tekst);
+                    Scene stseen2 = new Scene(label, 300, 100, Color.AQUAMARINE);
+                    uus.setScene(stseen2);
+                    uus.show();
+
+                } // kui visete arv sobib vahemikku, alustab mänguga.
+                catch (NumberFormatException e) { // Püüab vea kinni ja läheb
+                    // peaakanasse tagasi
+                    peaLava.hide();
+                    Stage uus = new Stage();
+                    String tekst = null;
+                    tekst = "Visete arv peab olema täisarv";
+                    Button ok = new Button("Proovi uuesti"); // luuakse nupp
+                    // nupu paigutus annab soovida !!!
+                    Label label = new Label(tekst, ok);
+                    Scene stseen2 = new Scene(label, 300, 100, Color.AQUAMARINE);
+                    uus.setScene(stseen2);
+                    uus.show();
+                    ok.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        public void handle(MouseEvent me) {
+                            uus.hide();
+                            peaLava.show();
+                        }
+
+                    });
+
+                }
+
+            }
+        });
+
+        clear.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                mängijanr1.clear();
+                mängijanr2.clear();
+                viskeArvMängus.clear();
+            }
+        });
+        cancel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                peaLava.hide();
+                return; // peaks katkestama programmi siis
+            }
+        });
+
+
+
+        return grid;
+
+    }
+
     public static void main(String[] args) {
-
-        String sisestus = JOptionPane.showInputDialog(null, "Sisestage esimese mängija nimi : ", "Täringu mäng",
-                JOptionPane.QUESTION_MESSAGE); //Sisestuskast
-        if (sisestus == null)
-            return; // katkestab programmi, kui vajutatakse cancel
-        Mängija mängija1 = new Mängija(sisestus); //loome klassi Mängija isendi mängija1, mis saab ette siestatud nime.
-
-        sisestus = JOptionPane.showInputDialog(null, "Sisestage teise mängija nimi : ", "Täringu mäng",
-                JOptionPane.QUESTION_MESSAGE);
-        if (sisestus == null)
-            return;
-        Mängija mängija2 = new Mängija(sisestus);
-
-
-        // õpetus
-        System.out.println("Mängu eesmärgiks on võimalikult palju punkte koguda veeretades kahte täringut!");
-        System.out.println("Voorude arvu saab ise valida");
-        System.out.println("Kõige rohkem saab punkte visatest kaks täringut nii, et nende silmade arv oleks sama (+30)");
-        System.out.println("Punkte teenib ka siis kui täringute korrutis jagub mõne kolme astemga (vastavalt +3, +12, +20)");
-        System.out.println("Või siis kui summa on paaris (+8)");
-        System.out.println("Või summa/ korrutis lõppeb nulliga (+15)");
-        System.out.println("Punkte kaotab paaritu arvu viskamise eest (-4)");
-        System.out.println("Või siis kui korrutis ei jagu mõne kolme astmega (-1)");
-        System.out.println();
-
-
-
-
-
-        String viskeArvMängus;
-        do {
-            viskeArvMängus = JOptionPane.showInputDialog(null, "Sisestage visete arv (1-10) :", "Täringu mäng",
-                    JOptionPane.QUESTION_MESSAGE); //viseteArv on String tüüpi, sest ei osanud nii, et saaks kasti sisestada int tüüpi argumenti.
-            if (viskeArvMängus == null || viskeArvMängus.equals("")) //Stringe võib võrrelda ainult equaliga.
-                return; //katkestab programmi, kui vajutatakse cancel või ei sisestata midagi.
-            try { // Proovib kas sisestatud numbrit on võimalik int tüüpi muutujaks muuta.
-                int viseteArvInt = Integer.parseInt(viskeArvMängus);
-                if (viseteArvInt <= 10 && viseteArvInt > 0)
-                    break; //kui visete arv sobib vahemikku, katkestame programmi.
-            } catch (NumberFormatException e) { // Püüab vea kinni ja läheb uuesti küsima tänu while tsüklile.
-                 }
-
-        } while (true);
-
-
-        MänguKäik uusMäng = new MänguKäik(0,Integer.parseInt(viskeArvMängus),0);
-
-        uusMäng.alustaMänguga(mängija1, mängija2);
-
-
-        int summa1 = uusMäng.getPunktideSumma1();
-        int summa2 = uusMäng.getPunktideSumma2();
-
-        Võitja võitja = new Võitja();
-
-        String võitjanimi = võitja.leiaVõitja(mängija1, mängija2,summa1, summa2);
-
-        System.out.println(mängija1 + " punktisumma : " + summa1);
-        System.out.println(mängija2 + " punktisumma : " + summa2);
-
-        System.out.println(võitjanimi);
+        launch(args);
     }
 }
--- src/MänguKäik.java
+++ src/MänguKäik.java
@@ -1,10 +1,18 @@
-import javax.swing.*;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class MänguKäik {
     private String viseteArv = "";
     private int viseteArvInt;
     private int punktideSumma1 = 0;
     private int punktideSumma2 = 0;
+    private int clicks = 0;
 
     public MänguKäik(int punktideSumma1, int viseteArvInt, int punktideSumma2) {
         this.punktideSumma1 = punktideSumma1;
@@ -12,37 +20,50 @@ public class MänguKäik {
         this.punktideSumma2 = punktideSumma2;
     }
 
-    public void alustaMänguga(Mängija mängija1, Mängija mängija2){
-    for (int i = 1; i <= viseteArvInt; i++) { //Mäng käib seni, kuni sisestatud visete arv saab otsa.
-        JOptionPane.showMessageDialog(null, "Veereta", "Täringu mäng", JOptionPane.PLAIN_MESSAGE);
+    public void alustaMänguga(Mängija mängija1, Mängija mängija2, GridPane grid, Stage peaLava){
+        for (int i = 0; i <= viseteArvInt; i++)
+            peaLava.hide();
+        Stage uus = new Stage();
+        String tekst = null;
+        tekst = "MÄNG KÄIB !!";
+        Button nupp = new Button("Veereta"); // luuakse nupp
+        // nupu paigutus annab soovida !!!
+        Label label = new Label(tekst, nupp);//ilma tekstita ei saa
+        Scene stseen2 = new Scene(label, 300, 100, Color.AQUAMARINE);
+        uus.setScene(stseen2);
+        uus.show();
+        for (int i = 1; i <= viseteArvInt; i++) { //Mäng käib seni, kuni sisestatud visete arv saab otsa.
+            nupp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent me) {
+                    clicks++;
+                    System.out.println("Esimese mängija " + mängija1 + " viskab: ");
+                    mängija1.getTäringud().viska(); //kõigepealt võtame mängija alt täringu ja sealt suunatakse edasi klassi Täring, kus sooritatakse meetod viska()
+                    //System.out.println("Tema kahe täringu summa: " + mängija1.getTäringud().täringuteSumma());
+                    //System.out.println("Esimese mängija täringute korrutis " + mängija1.getTäringud().täringuteKorrutis());
 
 
-        System.out.println("Esimese mängija " + mängija1 + " viskab: ");
-        mängija1.getTäringud().viska(); //kõigepealt võtame mängija alt täringu ja sealt suunatakse edasi klassi Täring, kus sooritatakse meetod viska()
+                    System.out.println("Teine mängija " + mängija2 + " viskab: ");
+                    mängija2.getTäringud().viska();
 
-        //System.out.println("Tema kahe täringu summa: " + mängija1.getTäringud().täringuteSumma());
-        //System.out.println("Esimese mängija täringute korrutis " + mängija1.getTäringud().täringuteKorrutis());
+                    //System.out.println("Tema kahe täringu summa: " + mängija2.getTäringud().täringuteSumma());
+                    //System.out.println("Teise mängija täringute korrutis " + mängija2.getTäringud().täringuteKorrutis());
 
+                    System.out.println();
 
-        System.out.println("Teine mängija " + mängija2 + " viskab: ");
-        mängija2.getTäringud().viska();
+                    Punktid punktisumma = new Punktid(mängija1.getTäringud()); //loome klassi punktid punktisumma isendi, mis saab ette täringu väärtused
+                    System.out.println("Vaheseis: " + mängija1 + " punktisumma: " + punktisumma.arvutaPunktid()); //väljastab vaheseisu (palju punkte kellelgi on)
+                    punktideSumma1 += (punktisumma.arvutaPunktid()) / 2; //pean jagama kahegi, et tuleks õige vastus, millegi pärast ??
 
-        //System.out.println("Tema kahe täringu summa: " + mängija2.getTäringud().täringuteSumma());
-        //System.out.println("Teise mängija täringute korrutis " + mängija2.getTäringud().täringuteKorrutis());
-
-        System.out.println();
-
-        Punktid punktisumma = new Punktid(mängija1.getTäringud()); //loome klassi punktid punktisumma isendi, mis saab ette täringu väärtused
-        System.out.println("Vaheseis: " + mängija1 + " punktisumma: " + punktisumma.arvutaPunktid()); //väljastab vaheseisu (palju punkte kellelgi on)
-        punktideSumma1 += (punktisumma.arvutaPunktid()) / 2; //pean jagama kahegi, et tuleks õige vastus, millegi pärast ??
-
-        punktisumma = new Punktid(mängija2.getTäringud());
-        System.out.println("Vaheseis: " + mängija2 + " punktisumma: " + punktisumma.arvutaPunktid());
-        punktideSumma2 += (punktisumma.arvutaPunktid()) / 2;
-
-        System.out.println(i + " ring läbi");
-        System.out.println();
-    }
+                    punktisumma = new Punktid(mängija2.getTäringud());
+                    System.out.println("Vaheseis: " + mängija2 + " punktisumma: " + punktisumma.arvutaPunktid());
+                    punktideSumma2 += (punktisumma.arvutaPunktid()) / 2;
+                    if (clicks==viseteArvInt)
+                        uus.close();
+                }
+            });
+            System.out.println(i + " ring läbi");
+            System.out.println();
+        }
     }
 
     public String getViseteArv() {
